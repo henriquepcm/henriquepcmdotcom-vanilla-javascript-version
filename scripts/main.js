39ce860701b676cb849fbc2357143957733f7d05
Henrique Pochmann <henriquepochmann@Henriques-MacBook-Pro.local>
2023-12-05 11:43:29 -0300

    new hover effect for links and accessibility improvements

--- scripts/main.js
+++ scripts/main.js
@@ -12,19 +12,23 @@ const container = document.getElementById("container"); // Select <div id="conta
 const main = document.querySelector("main"); // Select <main>
 const sections = document.querySelectorAll("section[id^='section']"); // Select sections' IDs
 const footer = document.querySelector("footer");
+const getFocusableElements = document.querySelectorAll("a");
+
+//
+let liElement;
 
 function openMenu() {
-  menuBackground.classList.remove("-translate-x-full");
-  menuBackground.classList.add("translate-x-1/4");
-  body.classList.add("overflow-hidden");
-  html.classList.add("overflow-hidden");
+     menuBackground.classList.remove("-translate-x-full");
+     menuBackground.classList.add("translate-x-1/4");
+     body.classList.add("overflow-hidden");
+     html.classList.add("overflow-hidden");
 }
 
 function closeMenu() {
-  menuBackground.classList.add("-translate-x-full");
-  menuBackground.classList.remove("translate-x-1/4");
-  body.classList.remove("overflow-hidden");
-  html.classList.remove("overflow-hidden");
+     menuBackground.classList.add("-translate-x-full");
+     menuBackground.classList.remove("translate-x-1/4");
+     body.classList.remove("overflow-hidden");
+     html.classList.remove("overflow-hidden");
 }
 
 //Add event listener to the burger menu and menu's background
@@ -33,92 +37,89 @@ menuBackground.addEventListener("click", closeMenu);
 
 // Close the Menu if a click happens anywhere on the screen
 document.addEventListener("click", function (e) {
-  const isBurgerClick = burger.contains(e.target);
-  if (!isBurgerClick) {
-    closeMenu();
-  }
+     const isBurgerClick = burger.contains(e.target);
+     if (!isBurgerClick) {
+          closeMenu();
+     }
 });
 
-//Function to create menu links and link to their respective section
-document.addEventListener("DOMContentLoaded", function () {
-  //Function to hide all sections
-  function hideSections() {
-    sections.forEach((section) => {
-      section.classList.add("opacity-0", "duration-75", "ease-in");
-      section.classList.remove("delay-[900ms]", "duration-300", "opacity-100");
-      setTimeout(() => {
-        section.classList.add("hidden");
-      }, "100");
-    });
-  }
+//Function to hide all sections
+function hideSections() {
+     sections.forEach((section) => {
+          section.classList.add("opacity-0", "duration-75", "ease-in");
+          section.classList.remove(
+               "delay-[900ms]",
+               "duration-300",
+               "opacity-100"
+          );
+          setTimeout(() => {
+               section.classList.add("hidden");
+          }, "100");
+     });
+}
 
-  //Function to remove styles from menu items that didn't get clicked
-  function unmarkMenuItems() {
-    menuItems.forEach((item) => {
-      item.classList.remove("menu-item--selected");
-      item.classList.add("menu-item");
-    });
-  }
+//Function to remove styles from menu items that didn't get clicked
+function unmarkMenuItems() {
+     menuItems.forEach((item) => {
+          const liElement = item.parentNode;
+          liElement.classList.remove("menu-item-active");
+     });
+}
 
-  // Hide all sections except the one that got clicked
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetId = e.target.getAttribute("href").substring(1); // Get the value on href and removes the "#"
-      hideSections(); // hide all sections
+// Actions after menu click
+menuItems.forEach((menuItem) => {
+     menuItem.addEventListener("click", (e) => {
+          e.preventDefault();
+          const targetId = e.target.getAttribute("href").substring(1); // Get the value on href and removes the "#"
+          hideSections(); // hide all sections
+          unmarkMenuItems();
+          e.target.parentNode.classList.add("menu-item-active");
 
-      setTimeout(() => {
-        document.getElementById(targetId).classList.remove("hidden");
-      }, "110");
+          setTimeout(() => {
+               document.getElementById(targetId).classList.remove("hidden");
+          }, "110");
 
-      setTimeout(() => {
-        document.getElementById(targetId).classList.remove("opacity-0");
-        document
-          .getElementById(targetId)
-          .classList.add("opacity-100", "duration-75", "ease-in");
-      }, "120");
+          setTimeout(() => {
+               document.getElementById(targetId).classList.remove("opacity-0");
+               document
+                    .getElementById(targetId)
+                    .classList.add("opacity-100", "duration-75", "ease-in");
+          }, "120");
 
-      // Remove styles from menu items that didn't get clicked
-      unmarkMenuItems();
+          // Add specific set of styles for specific sections
+          if (
+               [
+                    "section-home",
+                    "section-about",
+                    "section-skills",
+                    "section-projects",
+               ].includes(targetId)
+          ) {
+               main.classList.add("items-center");
 
-      // Add styles to the menu item that got clicked
-      menuItem.classList.add("menu-item--selected");
+               // Add specific set of styles for the other sections
+          } else {
+               main.classList.remove("items-center");
+          }
+     });
+});
 
-      // Add specific set of styles for specific sections
-      if (
-        [
-          "section-home",
-          "section-about",
-          "section-skills",
-          "section-projects",
-        ].includes(targetId)
-      ) {
-        main.classList.add("items-center");
+//Add link to the logo
+logo.addEventListener("click", (e) => {
+     e.preventDefault();
+     hideSections();
+     unmarkMenuItems();
 
-        // Add specific set of styles for the other sections
-      } else {
-        main.classList.remove("items-center");
-      }
-    });
-  });
-  //Add link to the logo
-  logo.addEventListener("click", (e) => {
-    e.preventDefault();
-    hideSections(); // Hide all sections
-    unmarkMenuItems(); // Remove styles from menu items that didn't get clicked
+     setTimeout(() => {
+          sections[0].classList.remove("hidden");
+     }, "110");
 
-    setTimeout(() => {
-      sections[0].classList.remove("hidden");
-    }, "110");
+     setTimeout(() => {
+          sections[0].classList.add("opacity-100", "duration-75", "ease-in");
+          sections[0].classList.remove("opacity-0");
+     }, "120");
 
-    setTimeout(() => {
-      sections[0].classList.add("opacity-100", "duration-75", "ease-in");
-      sections[0].classList.remove("opacity-0");
-    }, "120");
-
-    menuItems[0].classList.add("menu-item--selected"); // Mark Home on the menu as selected
-    main.classList.add("items-center");
-  });
+     main.classList.add("items-center");
 });
 
 // Animations for Burger Menu, Logo, Footer and Sections when the page loads
@@ -126,3 +127,28 @@ burger.classList.remove("opacity-0", "-translate-y-full");
 logo.classList.remove("opacity-0", "-translate-y-5");
 footer.classList.remove("opacity-0", "translate-y-5");
 sections[0].classList.remove("opacity-0");
+
+//Keyboard Events for Accessibility
+const focusableElements = Array.from(getFocusableElements);
+
+function keyboardClicks(e) {
+     if (e.code === "Tab") {
+          openMenu();
+     } else if (e.code === "Escape") {
+          closeMenu();
+     } else if (e.code === "ArrowRight" || e.code === "ArrowDown") {
+          moveFocus(1);
+     } else if (e.code === "ArrowLeft" || e.code === "ArrowUp") {
+          moveFocus(-1);
+     }
+}
+
+function moveFocus(moveTo) {
+     const currentFocus = document.activeElement;
+     const currentIndex = focusableElements.indexOf(currentFocus);
+     const nextIndex =
+          (currentIndex + moveTo + focusableElements.length) %
+          focusableElements.length;
+     focusableElements[nextIndex].focus();
+}
+document.addEventListener("keydown", keyboardClicks);
